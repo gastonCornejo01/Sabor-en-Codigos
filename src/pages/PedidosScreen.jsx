79aee8f86038c22441cfe391679f83e1becdd2d6
Gaston Cornejo <gastocorne@gmail.com>
2022-08-09 20:11:28 -0300

    Rutas creadas

---
+++ src/pages/PedidosScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PedidosScreen = () => {
+  return (
+    <div>PedidosScreen</div>
+  )
+}
+
+export default PedidosScreen
---
+++ src/routes/RoutesApp.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import HomeScreen from '../pages/HomeScreen'
+import LoginScreen from '../pages/LoginScreen'
+import RegistroScreen from '../pages/RegistroScreen'
+import PedidosScreen from '../pages/PedidosScreen'
+import ErrorScreen from '../pages/ErrorScreen'
+
+const RoutesApp = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/registro" element={<RegistroScreen />} />
+        <Route path="/pedidos" element={<PedidosScreen />} />
+        <Route path="*" element={<ErrorScreen />} />
+    </Routes>
+
+    )
+}
+
+export default RoutesApp
