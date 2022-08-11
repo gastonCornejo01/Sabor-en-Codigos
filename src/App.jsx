25ebb9f1de66859d930e87ba76ced5c533e43ab0
Gaston Cornejo <gastocorne@gmail.com>
2022-08-11 01:42:19 -0300

    ultimos ajustes de la estructura principal del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import './App.css'
+import Navbar from "./components/Navbar";
 import RoutesApp from "./routes/RoutesApp";
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
     <RoutesApp />
     </BrowserRouter>
   )
